7f0622fdd9226f5aabae274ada74790581c6db60
Paddy Walsh <WalshP@LT0585.local>
2022-09-17 09:13:21 +0100

    refactor navigation to seperate files

--- App.js
+++ App.js
@@ -1,25 +1,10 @@
 import * as React from 'react';
-import {Button, View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from './src/components/home/home';
 import DetailsScreen from './src/components/details/details';
 import SettingsScreen from './src/components/settings/settings';
-
-function HomeScreen({navigation}) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
-      <Button
-        title="Go to Settings"
-        onPress={() => navigation.navigate('Settings')}
-      />
-    </View>
-  );
-}
+import RulesScreen from './src/components/rules/rules';
 
 const Stack = createNativeStackNavigator();
 
@@ -34,6 +19,7 @@ function App() {
         />
         <Stack.Screen name="Details" component={DetailsScreen} />
         <Stack.Screen name="Settings" component={SettingsScreen} />
+        <Stack.Screen name="Rules" component={RulesScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
